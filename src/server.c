c62f944658d850eb60996884739a704b594e1e05
tezc <ozantezcan@gmail.com>
2021-10-05 14:16:41 +0300

    Add sanitizer support and clean up sanitizer findings

--- src/server.c
+++ src/server.c
@@ -4921,6 +4921,8 @@ int prepareForShutdown(int flags) {
     if (server.supervised_mode == SUPERVISED_SYSTEMD)
         redisCommunicateSystemd("STOPPING=1\n");
 
+    if (server.sentinel_mode) freeSentinel();
+
     /* Kill all the Lua debugger forked sessions. */
     ldbKillForkedSessions();
 
