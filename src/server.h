26ee5097b5a55395075e8518183b6cf078e3e311
tezc <ozantezcan@gmail.com>
2021-11-02 14:13:59 +0300

    fix ub

--- src/server.h
+++ src/server.h
@@ -2940,7 +2940,7 @@ void disableWatchdog(void);
 void watchdogScheduleSignal(int period);
 void serverLogHexDump(int level, char *descr, void *value, size_t len);
 
-REDIS_NO_SANITIZE("undefined");
+REDIS_NO_SANITIZE("undefined")
 int memtest_preserving_test(unsigned long *m, size_t bytes, int passes);
 void mixDigest(unsigned char *digest, void *ptr, size_t len);
 void xorDigest(unsigned char *digest, void *ptr, size_t len);
