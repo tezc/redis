e957dfe12f201d2c50ed4c34edb5e6e33c128b0d
Ozan Tezcan <ozantezcan@gmail.com>
2025-06-19 11:03:00 +0300

    diagram

--- src/cluster_asm.h
+++ src/cluster_asm.h
@@ -22,6 +22,35 @@ void clusterSyncSlotsCommand(client *c);
 
 
 /* API for implementation/plugin */
+/*
+┌───────────────┐            ┌───────────────┐         ┌───────────────┐             ┌───────────────┐
+│ Destination   │            │ Destination   │         │    Source     │             │ Source        │
+│ Cluster plugin│            │ Master        │         │    Master     │             │ Cluster plugin│
+└───────┬───────┘            └───────┬───────┘         └───────┬───────┘             └───────┬───────┘
+        │                            │                         │                             │
+        │ ASM_REQUEST_IMPORT_START   │                         │                             │
+        ├───────────────────────────►│                         │                             │
+        │                            │ CLUSTER SYNCSLOTS <arg> │                             │
+        │                            ├────────────────────────►│                             │
+        │                            │                         │                             │
+        │                            │  SNAPSHOT(restore cmds) │                             │
+        │                            │◄────────────────────────┤                             │
+        │                            │  Repl stream            │                             │
+        │                            │◄────────────────────────┤                             │
+        │                            │                         │ ASM_EVENT_IMPORT_WAIT_PAUSE │
+        │                            │                         ├────────────────────────────►│
+        │                            │                         │  ASM_REQUEST_IMPORT_PAUSED  │
+        │                            │                         │◄────────────────────────────┤
+        │                            │ Drain repl stream       │                             │
+        │                            │◄────────────────────────┤                             │
+        │ ASM_EVENT_IMPORT_COMPLETED │                         │                             │
+        │◄───────────────────────────┤                         │                             │
+        │                            │                         │                             │
+        │clusterNotifyConfigUpdated()│                         │                             │
+        ├───────────────────────────►│                         │ clusterNotifyConfigUpdated()│
+        │                            │                         │◄────────────────────────────┤
+        │                            │                         │                             │
+ */
 
 #define ASM_REQUEST_IMPORT_START      1  /* Start a new import operation (destination side) */
 #define ASM_REQUEST_IMPORT_CANCEL     2  /* Cancel an ongoing import operation (destination side) */
@@ -44,6 +73,8 @@ int clusterAsmOnEvent(slotRangeArray *slot_ranges, int event, void *arg);
 /* Both on source and destination side. Implementation calls when the config
  * is updated */
 int clusterNotifyConfigUpdated(slotRangeArray *slot_ranges, sds *err);
+
+
 
 
 #endif
