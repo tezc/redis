067b637de0b8b9e61c6e5b0737a9f10f5a5455d9
Ozan Tezcan <ozantezcan@gmail.com>
2025-07-06 07:02:26 +0300

    test log

--- tests/unit/cluster/atomic-slot-migration.tcl
+++ tests/unit/cluster/atomic-slot-migration.tcl
@@ -188,11 +188,14 @@ start_cluster 3 3 {tags {external:skip cluster}} {
             }
 
             # The task should be failed due to the fail point
-            wait_for_condition 1000 50 {
+            wait_for_condition 1000 5 {
                 [string match "*$channel*${state}*" [migration_status 0 0-100 error]] ||
                 [string match "*$channel*${state}*" [migration_status 1 0-100 error]]
             } else {
-                fail "ASM task did not fail with expected error"
+                fail "ASM task did not fail with expected error -
+                     (src: [migration_status 0 0-100 error],
+                      dst: [migration_status 0 0-100 error],
+                      expected: $channel $state)"
             }
             R 1 config set rdb-key-save-delay 0
             stop_write_load $load_handle
