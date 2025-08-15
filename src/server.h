ff632b6cff2ee13b45f5b3336fd6b8c27e7f6954
Yuan Wang <yuan.wang@redis.com>
2025-08-15 19:15:39 +0800

    Use AOF format to import data (#53)

--- src/server.h
+++ src/server.h
@@ -3206,6 +3206,7 @@ int aofDelHistoryFiles(void);
 int aofRewriteLimited(void);
 void updateCurIncrAofEndOffset(void);
 void updateReplOffsetAndResetEndOffset(void);
+int rewriteObject(rio *r, robj *key, robj *o, int dbid, long long expiretime);
 
 /* Child info */
 void openChildInfoPipe(void);
